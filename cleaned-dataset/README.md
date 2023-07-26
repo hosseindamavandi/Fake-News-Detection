01154be7287e33edc9e7cd496459a3594dc68aa3
Andrew Muhsen Paramedis <Andrew.naaem99@eng-st.cu.edu.eg>
2023-07-26 17:05:14 +0300

    Revert "Merge branch 'main' of https://github.com/hosseindamavandi/Fake-News-Detection"
    This reverts commit 71483fbad19e3d25b88f45a354b38c54ec2e36aa, reversing
    changes made to 2b97f60ea8a72c00226aa5caafd838d03da1919f.

--- cleaned-dataset/README.md
+++ cleaned-dataset/README.md
@@ -1,6 +1,6 @@
 # Cleaned Dataset
 This file is generated after applying preprocessing on the [raw dataset](https://github.com/hosseindamavandi/Fake-News-Detection/tree/main/datasets).
-It's available [here](https://drive.google.com/file/d/1AHv8HLZ_L2ACIa6Jp16b-3dImluxezle/view?usp=sharing)
+It's available [here](https://drive.google.com/drive/folders/1uh5m2VfweabRUi1DMcjORpWAXGh668am?usp=drive_link )
 
 ## Fetching the dataset
 - Fetch data from the above link and place it in the `cleaned-dataset` folder.
--- preprocess.py
+++ preprocess.py
@@ -23,68 +23,6 @@ def preprocess_text(text):
 
     return stemmed_tokens
 
-def remove_URL(text):
-    url = re.compile(r'https?://\S+')
-    return url.sub(r' httpsmark ', text)
-
-def remove_html(text):
-    html = re.compile(r'<.*?>')
-    return html.sub(r'', text)
-
-def remove_atsymbol(text):
-    name = re.compile(r'@\S+')
-    return name.sub(r' atsymbol ', text)
-
-def remove_hashtag(text):
-    hashtag = re.compile(r'#')
-    return hashtag.sub(r' hashtag ', text)
-
-def remove_exclamation(text):
-    exclamation = re.compile(r'!')
-    return exclamation.sub(r' exclamation ', text)
-
-def remove_question(text):
-    question = re.compile(r'?')
-    return question.sub(r' question ', text)
-
-def remove_emoji(string):
-    emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                               u"\U00002500-\U00002BEF"  # chinese char
-                               u"\U00002702-\U000027B0"
-                               u"\U00002702-\U000027B0"
-                               u"\U000024C2-\U0001F251"
-                               u"\U0001f926-\U0001f937"
-                               u"\U00010000-\U0010ffff"
-                               u"\u2640-\u2642"
-                               u"\u2600-\u2B55"
-                               u"\u200d"
-                               u"\u23cf"
-                               u"\u23e9"
-                               u"\u231a"
-                               u"\ufe0f"  # dingbats
-                               u"\u3030"
-                               "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r' emoji ', string)
-
-def remove_number(text):
-    number = re.compile(r'\d+')
-    return number.sub(r' number ', text)
-
-def remove_nulls(data):
-    indices = []
-    for i in range(len(data)):
-        try:
-            if len(data.iloc[i]['text_vector']) != 100:
-                print(i, len(data.iloc[i]['text_vector']))
-        except:
-            indices.append(i)
-
-    new_data = data.drop(indices)
-    return new_data
 
 def vectorize_text(text, model):
     vectors = [model.wv[word] for word in text if word in model.wv]
