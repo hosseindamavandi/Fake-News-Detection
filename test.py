2b6be62c685e3c2d1be4acfe976dd37cc6c72c0a
Andrew Muhsen Paramedis <Andrew.naaem99@eng-st.cu.edu.eg>
2023-07-28 11:31:54 +0300

    path fix

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from preprocess import *
 from models import ANN, CNN1D, BILSTM
 from torch.utils.data import Dataset, DataLoader
 import gensim
+import argparse
 
 INPUT_SIZE = 100
 HIDDEN_STATE = 64
@@ -15,38 +16,21 @@ NUM_CLASSES = 1 #* binary classification
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-path_word2vec = "C:\\Users\\DELL\\OneDrive\\Desktop\\AI projects\\Fake-News-Detection\\word2vec_model\\word2vec.model"
+
 
 path_pytorch_ANN = r'model_files\ANN_best_model.pth'
 path_pytorch_CNN1D = r'model_files\CNN1D_best_model.pth'
 path_pytorch_LSTM = r'model_files\BILSTM_best_model.pth'
 
-try :
-    word2vec_model = gensim.models.Word2Vec.load(path_word2vec)
-except FileNotFoundError:
-    path_word2vec = r'/content/drive/MyDrive/Neuromatch/ANN/temp/word2vec_model/word2vec.model'
-    word2vec_model = gensim.models.Word2Vec.load(path_word2vec)
-
-
-
-
-
-
-# # sample = preprocess_text(sample)
-# # sample = vectorize_text(sample, word2vec_model)
-# # sample = torch.tensor(sample).float().to(DEVICE).unsqueeze(0).unsqueeze(1)
-# # print(sample.shape)
-# # model.eval()
-# # model(sample)
-# # print(predict(sample, word2vec_model, loaded_ann, DEVICE))
-
-
-import argparse
 
 
 if __name__ == "__main__":
+    local_path = r"C:\\Users\\DELL\\OneDrive\\Desktop\\AI projects\\Fake-News-Detection\\word2vec_model\\word2vec.model"
+    colab_path = r'/content/drive/MyDrive/Neuromatch/ANN/temp/word2vec_model/word2vec.model'
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="ANN")
+    parser.add_argument("--colab", type=bool, default=False)
     args = parser.parse_args()
     
     if args.model == "ANN":
@@ -62,6 +46,12 @@ if __name__ == "__main__":
         model.load_state_dict(torch.load(path_pytorch_LSTM))
     else:
         raise Exception("Invalid model name, please choose one of the following: ANN, CNN1D, LSTM, BILSTM")
+    
+    
+    if args.colab:
+        word2vec_model = gensim.models.Word2Vec.load(colab_path)
+    else:
+        word2vec_model = gensim.models.Word2Vec.load(local_path)
 
     print(f"{model._get_name()} model loaded successfully\n")
     sample = input("Enter a news: ")
